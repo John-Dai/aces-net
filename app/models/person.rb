b86380058ef4ab1c6081eb4d712795273fbcffb6
John Dai <johnrundai@gmail.com>
2015-11-08 03:01:20 -0500

    added eail and pw validators

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,8 @@
 class Person < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :email, presence: true, uniqueness: true
+  validates :password, length: { in: 8..72 }
   has_many :courses, dependent: :destroy
   include Amistad::FriendModel
   devise :database_authenticatable, :registerable,
